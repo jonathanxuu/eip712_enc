895f811bc51d3eac572064fd2ab3d246921144f2
Kevin Wang <wy721@qq.com>
2024-05-21 09:15:33 +0800

    test: add deserialization tests for EIP712 without chainId

--- src/eip712.rs
+++ src/eip712.rs
@@ -265,4 +265,82 @@ mod tests {
         let data = from_str::<EIP712>(string).unwrap();
         assert_eq!(data.validate().is_err(), true);
     }
+
+    #[test]
+    fn test_deserialization_no_chainid() {
+        let string = r#"{
+			"primaryType": "Mail",
+			"domain": {
+				"name": "Ether Mail",
+				"version": "1"
+			},
+			"message": {
+				"from": {
+					"name": "Cow",
+					"wallet": "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"
+				},
+				"to": {
+					"name": "Bob",
+					"wallet": "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"
+				},
+				"contents": "Hello, Bob!"
+			},
+			"types": {
+				"EIP712Domain": [
+					{ "name": "name", "type": "string" },
+					{ "name": "version", "type": "string" }
+				],
+				"Person": [
+					{ "name": "name", "type": "string" },
+					{ "name": "wallet", "type": "address" }
+				],
+				"Mail": [
+					{ "name": "from", "type": "Person" },
+					{ "name": "to", "type": "Person" },
+					{ "name": "contents", "type": "string" }
+				]
+			}
+		}"#;
+        let data = from_str::<EIP712>(string).unwrap();
+        assert!(data.validate().is_ok());
+    }
+
+    #[test]
+    fn test_deserialization_from_value() {
+        let value = serde_json::json!({
+            "primaryType": "Mail",
+            "domain": {
+                "name": "Ether Mail",
+                "version": "1"
+            },
+            "message": {
+                "from": {
+                    "name": "Cow",
+                    "wallet": "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"
+                },
+                "to": {
+                    "name": "Bob",
+                    "wallet": "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"
+                },
+                "contents": "Hello, Bob!"
+            },
+            "types": {
+                "EIP712Domain": [
+                    { "name": "name", "type": "string" },
+                    { "name": "version", "type": "string" }
+                ],
+                "Person": [
+                    { "name": "name", "type": "string" },
+                    { "name": "wallet", "type": "address" }
+                ],
+                "Mail": [
+                    { "name": "from", "type": "Person" },
+                    { "name": "to", "type": "Person" },
+                    { "name": "contents", "type": "string" }
+                ]
+            }
+        });
+        let data = serde_json::from_value::<EIP712>(value).unwrap();
+        assert!(data.validate().is_ok());
+    }
 }
